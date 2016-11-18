57474223193edec3f18e98a3ff427ace78462bc9
LeadroyaL <243850435@qq.com>
2016-11-18 10:38:57 +0800

    Initial commit

---
+++ app/src/main/java/com/leadroyal/isee/healthhelper/ChooseDeviceActivity.java
@@ -0,0 +1,13 @@
+package com.leadroyal.isee.healthhelper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChooseDeviceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_device);
+    }
+}
